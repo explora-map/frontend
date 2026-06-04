d7faf1c7bc17a4142f3601944e46c20f16db4d5e
asredes <asredes@protonmail.com>
2026-06-04 02:19:15 +0200

    fix: logo

--- src/components/AppLogo.jsx
+++ src/components/AppLogo.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logoDark from '../assets/images/logo-dark.png';
-import logoLight from '../assets/images/logo-light.png';
+import iconSvg from '../assets/images/logo.svg';
 
-export default function AppLogo({ className }) {
-    return (
-        <picture>
-            <source srcSet={logoDark} media="(prefers-color-scheme: dark)" />
-            <img src={logoLight} alt="Explora" className={className} />
-        </picture>
-    );
+export default function AppLogo({ className = '' }) {
+  return (
+    <div className={`app-logo ${className}`}>
+      <img src={iconSvg} alt="" aria-hidden="true" />
+      <span aria-hidden="true">Explora</span>
+    </div>
+  );
 }
