95c540f3b257834ab37cf6e4697c4cec35170001
asredes <asredes@protonmail.com>
2026-06-07 16:21:14 +0200

    fix: crud marcadores tras refactorizacion css

--- src/pages/MapaPrincipalPage.jsx
+++ src/pages/MapaPrincipalPage.jsx
@@ -716,6 +716,7 @@ export default function MapaPrincipalPage() {
                     onLocationSelect={
                         (mapaIdEngadindo && !mostrarFormMarcador)
                             ? ({ lat, lng }) => {
+                                console.log('[DEBUG] onLocationSelect chamado', lat, lng);
                                 setCoordsNovaMarcador({ lat, lng });
                                 setMostrarFormMarcador(true);
                             }
