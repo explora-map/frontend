334390bb9a1cd8132bdf269b27bf5e5d95eeab0f
asredes <asredes@protonmail.com>
2026-06-08 12:03:17 +0200

    fix: ruta logo

--- src/components/AppLogo.jsx
+++ src/components/AppLogo.jsx
@@ -1,9 +1,7 @@
-import iconSvg from '../assets/images/logo.svg';
-
 export default function AppLogo({ className = '' }) {
   return (
     <div className={`app-logo ${className}`}>
-      <img src={iconSvg} alt="" aria-hidden="true" />
+      <img src="/logo.svg" alt="" aria-hidden="true" />
       <span aria-hidden="true">Explora</span>
     </div>
   );
